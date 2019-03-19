e1384a9f8be1a97784695eaf83f3cc060cbd73c8
xiedong11 <xiedong11@aliyun.com>
2019-03-19 21:41:46 +0800

    初始化lib

---
+++ base/src/main/java/com/zhuandian/base/BaseAdapter.java
@@ -0,0 +1,48 @@
+package com.zhuandian.base;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * desc :
+ * author：xiedong
+ * date：2019/3/5
+ */
+public abstract class BaseAdapter<T, K extends BaseViewHolder> extends RecyclerView.Adapter<K> {
+
+    private List<T> mDatas;
+    private Context mContext;
+
+    public BaseAdapter(List<T> mDatas, Context context) {
+        this.mDatas = mDatas;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public K onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return (K) new BaseViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(getItemLayoutId(), viewGroup, false));
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull K myViewHolder, int position) {
+        converData(myViewHolder, mDatas.get(position), position);
+    }
+
+    protected abstract void converData(BaseViewHolder myViewHolder, T t, int position);
+
+    public abstract int getItemLayoutId();
+
+    @Override
+    public int getItemCount() {
+        return mDatas == null ? 0 : mDatas.size();
+    }
+
+
+}
---
+++ base/src/main/java/com/zhuandian/base/BaseViewHolder.java
@@ -0,0 +1,14 @@
+package com.zhuandian.base;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * desc :抽取通用ViewHodler
+ * author：xiedong
+ */
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+}
